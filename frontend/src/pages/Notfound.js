b9dcb05180af861a6a09716706726162df54cd83
abhinavdeshmukh2101 <63723008+abhinavdeshmukh2101@users.noreply.github.com>
2022-10-22 14:44:19 +0530

    .

---
+++ frontend/src/pages/Notfound.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const NotFound = () => {
+  return <div>404 Not Found</div>;
+};
+
+const Body = styled.div``;
+
+export default NotFound;
